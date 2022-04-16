308a19ec768b9c1d3fa20f8cac6d262b6e969c2b
luisrafaelo <99060582+luisrafaelo@users.noreply.github.com>
2022-04-16 18:42:09 -0400

    version 1.1

---
+++ Programas_6/Algoritmo_2.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+ 
+class Quadratic{
+   
+public:
+// ecuacion ax * 2 + bx + x
+//encuentra raices
+void findRoots(int a, int b, int c)
+{
+     
+    // Si a es 0, entonces la ecuacion
+    // no es cuadratica, pero si es lineal
+    if (a == 0)
+    {
+        cout << "No es cuadratica\n";
+        return;
+    }
+ 
+    int d = b * b - 4 * a * c;
+    float sqrt_val = sqrt(abs(d));
+ 
+    // Raices reales
+    if (d > 0)
+    {
+       cout << "Las raices son reales y diferentes" << endl;
+       cout << fixed << setprecision(1)
+            << float((-b + sqrt_val) / (2 * a)) << endl;
+       cout << fixed << setprecision(1)
+            << float((-b - sqrt_val) / (2 * a)) << endl;
+    }
+ 
+    // Imaginary Roots
+    else
+    {
+        cout << "Las raices son complejas" << endl;
+        cout << fixed <<setprecision(1)
+             << float(b / (2.0 * a)) << " + i"
+             << sqrt_val << endl;
+        cout << fixed << setprecision(1)
+             << float(b / (2.0 * a)) << " - i"
+             << sqrt_val << endl;
+    }
+}
+};
+ 
+//corre el codigo
+int main()
+{
+    Quadratic obj;
+ 
+    // Valor dado en los coeficientes
+    int a, b, c;
+    cout<<"\n\n\t\t Algoritmo numero 2\n";
+    cout<<"\n\n\tEcuacion cuadratica de la forma ax^2+bx+c=0\n";
+   cout<<"\ningrese a\n: ";
+   cin>>a; 
+    cout<<"\ningrese b\n:";
+   cin>>b;
+    cout<<"\ningrese c\n:";
+   cin>>c;
+    obj.findRoots(a, b, c);
+}

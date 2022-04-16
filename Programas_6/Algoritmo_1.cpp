e6f61882fd1632fd25fe4a7dba0c411390e70604
luisrafaelo <99060582+luisrafaelo@users.noreply.github.com>
2022-04-16 18:41:09 -0400

    version 1.1

--- Programas_6/Algoritmo_1.cpp
+++ Programas_6/Algoritmo_1.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
+char StopApp;
+// Funcion recursiva con static
+// variables p y f
+double e(int x, int n)
+{
+    static double p = 1, f = 1;
+    double r;
+ 
+    //Condicion de terminacion
+    if (n == 0)
+        return 1;
+ 
+    // Llamada recursiva
+    r = e(x, n - 1);
+    p = p * x;
+    // Factorial
+    f = f * n;
+    return (r + p / f);
+}
+ 
+// Corre el codigo
+int main()
+{
 
+    int x=0, n = 18;
+    cout<<"\n\n\t\t Algoritmo numero 1\n";
+    cout<<"\nIngrese el valor de x para e^x\n ";
+    cin>>x;
+    do{
+    cout<<"\n e^"<<x<<" = "<< e(x, n);
+cout<<"\nPara reiniciar la APP ingrese 'Y' (Si), si no quiere ingrese cualquier tecla\n";
+        cin>>StopApp;
+    }
+    while(toupper(StopApp) == 'Y');
+    return 0;
+}
+ 
